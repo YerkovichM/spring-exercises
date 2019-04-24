f0240da299f01227137904729de330f1e83b2821
tboychuk <tarass.boychuk@gmail.com>
2019-04-24 22:49:17 +0300

    Add new method to TestDataGenerator.java

--- spring-exercises-util/src/main/java/ua/procamp/TestDataGenerator.java
+++ spring-exercises-util/src/main/java/ua/procamp/TestDataGenerator.java
@@ -3,11 +3,14 @@ package ua.procamp;
 
 import io.codearte.jfairy.Fairy;
 import io.codearte.jfairy.producer.person.Person;
+import ua.procamp.model.Account;
+import ua.procamp.model.Gender;
 import ua.procamp.model.jpa.Address;
 import ua.procamp.model.jpa.Role;
 import ua.procamp.model.jpa.RoleType;
 import ua.procamp.model.jpa.User;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,6 +21,26 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class TestDataGenerator {
+
+    public Account generateAccount(){
+        Fairy fairy = Fairy.create();
+        Person person = fairy.person();
+        Random random = new Random();
+
+        Account fakeAccount = new Account();
+        fakeAccount.setFirstName(person.getFirstName());
+        fakeAccount.setLastName(person.getLastName());
+        fakeAccount.setEmail(person.getEmail());
+        fakeAccount.setBirthday(LocalDate.of(
+                person.getDateOfBirth().getYear(),
+                person.getDateOfBirth().getMonthOfYear(),
+                person.getDateOfBirth().getDayOfMonth()));
+        fakeAccount.setGender(Gender.valueOf(person.getSex().name()));
+        fakeAccount.setBalance(BigDecimal.valueOf(random.nextInt(200_000)));
+        fakeAccount.setCreationTime(LocalDateTime.now());
+
+        return fakeAccount;
+    }
 
     private List<Role> generateRoleList() {
         Random random = new Random();
